613aa0b92e5ee94305f4fa7398d12ede1e974364
Snoopy <email.ryan.uses.for.spam@gmail.com>
2024-02-10 01:13:16 -0500

    Adding a User-Agent for some websites that bitch about it.

--- wikifetch/app.rb
+++ wikifetch/app.rb
@@ -7,15 +7,10 @@ puts "Enter a URL: "
 
 # User Input
 userlink = gets.chomp
-
-
-
+  
 # Takes info from user input parses the html from a url, turns paragraphs into the paragraph bodies
-parsed_data = Nokogiri::HTML.parse(URI.open(userlink))
+parsed_data = Nokogiri::HTML.parse(URI.open(userlink, "User-Agent" => "EMAIL"))
 parsed_data.class
 paragraphs = parsed_data.css("p")
 
 writedata = File.write('pbodydata.txt', paragraphs)
-
-
-# Nokogiri::HTML::Document
